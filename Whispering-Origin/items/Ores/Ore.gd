fcc6c0aa7a0836c8aedb84010d26646d1c1188ed
Mauritz <mauritz@europe.com>
2019-10-09 01:38:43 +0300

    Update items, to make them more dynamic
    This update removes a lot of elements from the editor, and instead adds them through code, so that all items automatically have the same audio, timers and etc.

--- Whispering-Origin/items/Ores/Ore.gd
+++ Whispering-Origin/items/Ores/Ore.gd
@@ -5,54 +5,72 @@ signal drop_xp(skill_key, amount)
 signal to_low_level(skill_key, required_level)
 enum Dir { LEFT, RIGHT }
 var durability = 100
+var spawned = true
 
 export (String) var skill_key = "mining"
 export (String) var drop_item = "ore_copper"
 export (int) var required_skill_level = 1
+export (int) var respawn_time = 3
 export (int) var drop_item_amount_min = 1
 export (int) var drop_item_amount_max = 5
 export (int) var drop_xp_amount_per_item = 1
 
 var rng = RandomNumberGenerator.new()
+var pickaxe_sound_player = AudioStreamPlayer2D.new()
+var pickaxe_sound_sfx = preload("res://audio/pickaxe.ogg")
+var respawn_timer = Timer.new()
+var animation_player = AnimationPlayer.new()
+
 func _ready():
 	rng.randomize()
 	self.add_to_group("world_items")
 	
+	respawn_timer.wait_time = respawn_time
+	respawn_timer.connect("timeout", self, "_on_respawn_timer_timeout")
+	self.add_child(respawn_timer)
+	self.add_child(animation_player)
+	
+	pickaxe_sound_sfx.set_loop(false)
+	pickaxe_sound_player.stream = pickaxe_sound_sfx
+	self.add_child(pickaxe_sound_player)
+	
 	add_shake_animation()
 	add_standing_animation()
-	$AnimationPlayer.play("standing")
+	animation_player.play("standing")
 
 func hit(hitter):
 	if PlayerData.skills.has(self.skill_key):
 		if PlayerData.skills[self.skill_key].level >= self.required_skill_level:
-			if durability > 0:
+			if durability > 0 and spawned == true:
 				durability -= hitter.tools.pickaxe.hit_force
 				shake()
-				$AudioStreamPlayer2D.play(0.0)
+				pickaxe_sound_player.play(0.0)
 				
 				if durability <= 0:
 					$TextureRect/OreSprite.frame += 1
 					var items_dropped = rng.randi_range(self.drop_item_amount_min, self.drop_item_amount_max)
 					self.emit_signal("drop_item", self.drop_item, items_dropped)
 					self.emit_signal("drop_xp", self.skill_key, (items_dropped * self.drop_xp_amount_per_item))
-				$Timer.start()
+					spawned = false
+					respawn_timer.start()
 		else:
 			self.emit_signal("to_low_level", self.skill_key, self.required_skill_level)
 
-func _on_Timer_timeout():
-	$Timer.stop()
+func _on_respawn_timer_timeout():
+	respawn_timer.stop()
 	durability = 100
+	spawned = true
 	$TextureRect/OreSprite.frame -= 1
 
 
 func shake():
-	var shake_animation = $AnimationPlayer.get_animation("shake")
+	var shake_animation = animation_player.get_animation("shake")
 	if shake_animation:
 		var current_shake_value = shake_animation.track_get_key_value(0, 1)
 		shake_animation.track_set_key_value(0, 1, (current_shake_value * -1))
 		
 		if current_shake_value != null:
-			$AnimationPlayer.play("shake")
+			animation_player.play("shake")
 
 ########################################## ANIMATIONS ##########################################
 
@@ -65,7 +83,7 @@ func add_shake_animation():
 	animation.track_insert_key(rotation_track, 0.0, 0)
 	animation.track_insert_key(rotation_track, 0.1, -2)
 	animation.track_insert_key(rotation_track, 0.2, 0)
-	$AnimationPlayer.add_animation("shake", animation)
+	animation_player.add_animation("shake", animation)
 
 func add_standing_animation():
 	var animation = Animation.new()
@@ -75,4 +93,4 @@ func add_standing_animation():
 	animation.track_set_path(rotation_track, "TextureRect:rect_rotation")
 	animation.track_insert_key(rotation_track, 0.0, 0)
 	animation.track_insert_key(rotation_track, 0.5, 0)
-	$AnimationPlayer.add_animation("standing", animation)
+	animation_player.add_animation("standing", animation)
--- Whispering-Origin/items/Trees/Tree.gd
+++ Whispering-Origin/items/Trees/Tree.gd
@@ -5,33 +5,49 @@ signal drop_xp(skill_key, amount)
 signal to_low_level(skill_key, required_level)
 enum Dir { LEFT, RIGHT }
 var durability = 100
+var spawned = true
 
 export (String) var skill_key = "woodcutting"
 export (String) var drop_item = "log_standard"
 export (int) var required_skill_level = 1
+export (int) var respawn_time = 3
 export (int) var drop_item_amount_min = 1
 export (int) var drop_item_amount_max = 5
 export (int) var drop_xp_amount_per_item = 1
 
 var rng = RandomNumberGenerator.new()
+var chop_sound_player = AudioStreamPlayer2D.new()
+var chop_sound_sfx = preload("res://audio/chop.ogg")
+var fall_sound_player = AudioStreamPlayer2D.new()
+var fall_sound_sfx = preload("res://audio/tree_fall_2.ogg")
+var respawn_timer = Timer.new()
+var animation_player = AnimationPlayer.new()
+
 func _ready():
 	rng.randomize()
 	self.add_to_group("world_items")
 	#print(self.get_tree().get_root().get_node("."))
 	
+	respawn_timer.wait_time = respawn_time
+	respawn_timer.connect("timeout", self, "_on_respawn_timer_timeout")
+	animation_player.connect("animation_finished", self, "_on_animation_finished")
+	self.add_child(respawn_timer)
+	self.add_child(animation_player)
+	
+	add_sound()
 	add_shake_animation()
 	add_fall_animation()
 	add_fade_in_animation()
 	add_standing_animation()
-	$AnimationPlayer.play("standing")
+	animation_player.play("standing")
 
 func hit(hitter):
 	if PlayerData.skills.has(self.skill_key):
 		if PlayerData.skills[self.skill_key].level >= self.required_skill_level:
-			if durability > 0:
+			if durability > 0 and spawned == true:
 				durability -= hitter.tools.axe.hit_force
 				shake()
-				$ChopSound.play(0.0)
+				chop_sound_player.play(0.0)
 				
 				if durability <= 0:
 					if hitter.position.x > position.x:
@@ -41,38 +57,52 @@ func hit(hitter):
 					var items_dropped = rng.randi_range(self.drop_item_amount_min, self.drop_item_amount_max)
 					self.emit_signal("drop_item", self.drop_item, items_dropped)
 					self.emit_signal("drop_xp", self.skill_key, (items_dropped * self.drop_xp_amount_per_item))
-				$Timer.start()
+					spawned = false
+					respawn_timer.start()
 		else:
 			self.emit_signal("to_low_level", self.skill_key, self.required_skill_level)
 
-func _on_Timer_timeout():
-	$Timer.stop()
+func _on_animation_finished(animation_name):
+	if spawned == false and animation_name == "fade_in":
+		spawned = true
+
+func _on_respawn_timer_timeout():
+	respawn_timer.stop()
 	durability = 100
-	$AnimationPlayer.play("fade_in")
+	animation_player.play("fade_in")
 
 
 func shake():
-	var shake_animation = $AnimationPlayer.get_animation("shake")
+	var shake_animation = animation_player.get_animation("shake")
 	if shake_animation:
 		var current_shake_value = shake_animation.track_get_key_value(0, 1)
 		shake_animation.track_set_key_value(0, 1, (current_shake_value * -1))
 		
 		if current_shake_value != null:
-			$AnimationPlayer.play("shake")
+			animation_player.play("shake")
 
 func fall(direction):
-	var fall_animation = $AnimationPlayer.get_animation("fall")
+	var fall_animation = animation_player.get_animation("fall")
 	if fall_animation:
 		if direction == Dir.LEFT:
 			fall_animation.track_set_key_value(0, 1, -90)
 		else:
 			fall_animation.track_set_key_value(0, 1, 90)
-		#print(self.get_parent())
-		$FallSound.play(0.0)
-		$AnimationPlayer.play("fall")
+		fall_sound_player.play(0.0)
+		animation_player.play("fall")
 
-########################################## ANIMATIONS ##########################################
+########################################## ANIMATIONS & SOUND ##########################################
 
+func add_sound():
+	chop_sound_sfx.set_loop(false)
+	chop_sound_player.stream = chop_sound_sfx
+	self.add_child(chop_sound_player)
+	
+	fall_sound_sfx.set_loop(false)
+	fall_sound_player.stream = fall_sound_sfx
+	fall_sound_player.volume_db = -10
+	self.add_child(fall_sound_player)
+	
 func add_shake_animation():
 	var animation = Animation.new()
 	animation.length = 0.3
@@ -82,7 +112,7 @@ func add_shake_animation():
 	animation.track_insert_key(TextureRect_rotation_track, 0.0, 0)
 	animation.track_insert_key(TextureRect_rotation_track, 0.1, -2)
 	animation.track_insert_key(TextureRect_rotation_track, 0.2, 0)
-	$AnimationPlayer.add_animation("shake", animation)
+	animation_player.add_animation("shake", animation)
 	
 	var TextureRect_modulate_track = animation.add_track(Animation.TYPE_VALUE)
 	animation.track_set_path(TextureRect_modulate_track, "TextureRect:modulate")
@@ -115,7 +145,7 @@ func add_fall_animation():
 	animation.track_insert_key(TreeShadow_modulate_track, 0.4, Color(1, 1, 1, 1))
 	animation.track_insert_key(TreeShadow_modulate_track, 0.5, Color(1, 1, 1, 0))
 	
-	$AnimationPlayer.add_animation("fall", animation)
+	animation_player.add_animation("fall", animation)
 
 func add_fade_in_animation():
 	var animation = Animation.new()
@@ -136,7 +166,7 @@ func add_fade_in_animation():
 	animation.track_insert_key(TreeShadow_modulate_track, 0.0, Color(1, 1, 1, 0))
 	animation.track_insert_key(TreeShadow_modulate_track, 0.5, Color(1, 1, 1, 1))
 	
-	$AnimationPlayer.add_animation("fade_in", animation)
+	animation_player.add_animation("fade_in", animation)
 
 func add_standing_animation():
 	var animation = Animation.new()
@@ -157,4 +187,4 @@ func add_standing_animation():
 	animation.track_insert_key(TreeShadow_modulate_track, 0.0, Color(1, 1, 1, 1))
 	animation.track_insert_key(TreeShadow_modulate_track, 0.5, Color(1, 1, 1, 1))
 	
-	$AnimationPlayer.add_animation("standing", animation)
+	animation_player.add_animation("standing", animation)
